a5b6028a7b2e6125ae2d9e5771b9473af1043f3b
Zunnurhaq Zulkifli <zunnurhaqzulkifli@Zunnurhaqs-Air.realtek>
2023-10-14 19:01:00 +0800

    currency_converter_app

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPage();
+}
+
+class _CurrencyConverterMaterialPage
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0.00;
+  double currentUsdRate = 4.73;
+  double total = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  void convert() {
+    setState(() {
+      result = double.parse(textEditingController.text) * currentUsdRate;
+      (result != 0) ? total = result : total = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('rebuilt');
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 1,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(60),
+    );
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text(
+          'Currency Converter',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        // actions: const [
+        //   Text('Hello'),
+        // ],
+        leading: const Icon(Icons.abc),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'RM ${total.toStringAsFixed(2)}',
+                style: const TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+              TextField(
+                controller: textEditingController,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'RM 10.00',
+                  hintStyle: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: convert,
+                style: ElevatedButton.styleFrom(
+                  elevation: 5,
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Convertebrate',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
